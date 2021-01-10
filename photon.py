ff02db86aad993b9828c66084c34cd7f7ae6f5a2
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2021-01-10 21:50:46 +0900

    init commit

---
+++ photon.py
@@ -0,0 +1,367 @@
+# coding: UTF-8
+from collections import OrderedDict
+import urllib
+import urllib2
+import cookielib
+from bs4 import BeautifulSoup
+import hmac
+import hashlib
+import base64
+import uuid
+import json
+import requests
+
+class NoRedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_302(self, req, fp, code, msg, headers):
+        infourl = urllib.addinfourl(fp, headers, req.get_full_url())
+        infourl.status = code
+        infourl.code = code
+        return infourl
+    http_error_300 = http_error_302
+    http_error_301 = http_error_302
+    http_error_303 = http_error_302
+    http_error_307 = http_error_302
+
+class LobiAPI:
+	UserAgent 	= "photon-python"
+	platform 	= "android"
+	DeviceUUID 	= ""
+	Token 		= ""
+	def __init__(self):
+		opener = urllib2.build_opener(NoRedirectHandler(), urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+		urllib2.install_opener(opener)
+
+	def GetSpell(self, mail, password):
+		header = {
+			"User-Agent": "Mozilla/5.0 (Linux; Android 5.1; Google Nexus 10 - 5.1.0 - API 22 - 2560x1600 Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Safari/537.36 Lobi/8.10.3",
+			"Host": "lobi.co",
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+			"Accept-Language": "ja-JP,en-US;q=0.8",
+			"X-Requested-With": "com.kayac.nakamap"
+		}
+		source1 = urllib2.urlopen(urllib2.Request("https://lobi.co/inapp/signin/password?webview=1", None, header)).read()
+		soup = BeautifulSoup(source1, "html.parser")
+		csrf_token = soup.find('input', {'name':'csrf_token'})['value']
+		header["Referer"] = "https://lobi.co/inapp/signin/password?webview=1"
+		header["Origin"]  = "https://lobi.co"
+		post_data = {
+			"csrf_token": csrf_token,
+			"email": mail,
+			"password": password
+		}
+		res = urllib2.urlopen(urllib2.Request("https://lobi.co/inapp/signin/password", urllib.urlencode(post_data), header))
+		res_headers = res.info()
+		location = res_headers.getheader("Location")
+		key = "nakamapbridge://signin?spell="
+		if location != None and key in location and len(location) > len(key):
+			return location[len(key):]
+		return ""
+
+	def GetToken(self, device_uuid, spell):
+		sig = base64.b64encode(hmac.new("db6db1788023ce4703eecf6aa33f5fcde35a458c", spell, hashlib.sha1).digest())
+		header = {
+			"User-Agent": self.UserAgent,
+			"Host": "api.lobi.co"
+		}
+		post_data = {
+			"device_uuid": device_uuid,
+			"sig": sig,
+			"spell": spell,
+			"lang": "ja"
+		}
+		source = urllib2.urlopen(urllib2.Request("https://api.lobi.co/1/signin_confirmation", urllib.urlencode(post_data), header)).read()
+		result = json.loads(source)
+		return result["token"]
+
+	def Login(self, mail, password):
+		spell = self.GetSpell(mail, password)
+		if spell == None or spell == "":
+			return false
+		self.DeviceUUID = uuid.uuid4()
+		self.Token = self.GetToken(self.DeviceUUID, spell)
+		return self.Token != None and len(self.Token) > 0
+
+	def GetMe(self):
+		return self.GET(1, "me")
+
+	def GetContacts(self, user_id = None):
+		if user_id == None:
+			return self.GET(3, "me/contacts")
+		result = []
+		data = {}
+		while True:
+			res = self.GET(1, "user/{user_id}/contacts".format(user_id=user_id), data)
+			if res == None or res.get("users") == None or len(res["users"]) == 0:
+				break
+			result.extend(res["users"])
+			if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+				break
+			data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetFollowers(self, user_id = None):
+		if user_id == None:
+			return self.GET(2, "me/followers")
+		result = []
+		data = {}
+		while True:
+			res = self.GET(1, "user/{user_id}/followers".format(user_id=user_id), data)
+			if res == None or res.get("users") == None or len(res["users"]) == 0:
+				break
+			result.extend(res["users"])
+			if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+				break
+			data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetUser(self, user_id):
+		return self.GET(1, "user/{user_id}".format(user_id=user_id), { "fields": "is_blocked" })
+
+	def GetBlockingUsersAll(self):
+		result = []
+		data = {}
+		while True:
+			res = self.GET(2, "me/blocking_users", data)
+			if res == None or res.get("users") == None or len(res["users"]) == 0:
+				break
+			result.extend(res["users"])
+			if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+				break
+			data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetInvited(self):
+		return self.GET(1, "groups/invited")
+
+	def GetPublicGroupAll(self, uesr_id = None):
+		result = []
+		if user_id == None:
+			page = 1
+			while True:
+				res = self.GET(1, "public_groups", {
+					"with_archived": "true",
+					"count": 1000,
+					"page": page
+				})
+				page += 1
+				if res == None or len(res) == 0 or res[0] == None or res[0].get("items") == None or len(res[0]["items"]) == 0:
+					break
+				result.extend(res[0]["items"])
+				if len(res[0]["items"]) < 1000:
+					break
+		else:
+			result = []
+			data = { "with_archived": "true" }
+			while True:
+				res = self.GET(1, "user/{user_id}/visible_groups".format(user_id=user_id), data)
+				if res == None or res.get("public_groups") == None or len(res["public_groups"]) == 0:
+					break
+				result.extend(res["public_groups"])
+				if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+					break
+				data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetPublicGroup(self, page, count = 1000):
+		res = self.GET(1, "public_groups", {
+			"with_archived": "true",
+			"count": count,
+			"page": page
+		})
+		if res == None or len(res) == 0 or res[0] == None or res[0].get("items") == None or len(res[0]["items"]) == 0:
+			return []
+		return res[0]["items"]
+
+	def GetPrivateGroupAll(self):
+		result = []
+		page = 1
+		while True:
+			res = self.GET(3, "groups", {
+				"with_archived": "true",
+				"count": 1000,
+				"page": page
+			})
+			page += 1
+			if res == None or len(res) == 0 or res[0] == None or res[0].get("items") == None or len(res[0]["items"]) == 0:
+				break
+			result.extend(res[0]["items"])
+			if len(res[0]["items"]) < 1000:
+				break
+		return result
+
+	def GetPrivateGroup(self, page, count = 1000):
+		res = self.GET(3, "groups", {
+			"with_archived": "true",
+			"count": count,
+			"page": page
+		})
+		if res == None or len(res) == 0 or res[0] == None or res[0].get("items") == None or len(res[0]["items"]) == 0:
+			return []
+		return res[0]["items"]
+
+	def GetGroup(self, group_id):
+		return self.GET(2, "group/{group_id}".format(group_id=group_id), {
+			"members_count": 1,
+			"fields": "subleaders"
+		})
+
+	def GetGroupLeader(self, group_id):
+		return self.GET(2, "group/{group_id}/members".format(group_id=group_id), {
+			"members_count": 1,
+			"fields": "owner"
+		}).get("owner")
+
+	def GetGroupSubleaders(self, group_id):
+		res = self.GET(2, "group/{group_id}/members".format(group_id=group_id), {
+			"members_count": 1,
+			"fields": "owner"
+		}).get("subleaders")
+		if res == None:
+			return []
+		return res
+
+	def GetGroupMembersAll(self, group_id):
+		result = []
+		data = { "members_count": "1000" }
+		while True:
+			res = self.GET(1, "group/{group_id}/members".format(group_id=group_id), data)
+			if res == None or res.get("members") == None or len(res["members"]) == 0:
+				break
+			result.extend(res["members"])
+			if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+				break
+			data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetThreads(self, group_id, count = 20, older_than = None, newer_than = None):
+		data = { "count": count }
+		if older_than != None and older_than != "":
+			data["older_than"] = older_than
+		if newer_than != None and newer_than != "":
+			data["newer_than"] = newer_than
+		return self.GET(2, "group/{group_id}/chats".format(group_id=group_id), data)
+
+	def GetRepliesAll(self, group_id, chat_id):
+		return self.GET(1, "group/{group_id}/chats/replies".format(group_id=group_id), { "to": chat_id })
+
+	def GetPokesAll(self, group_id, chat_id):
+		result = []
+		data = { "id": chat_id }
+		while True:
+			res = self.GET(2, "group/{group_id}/chats/pokes".format(group_id=group_id), data)
+			if res == None or res.get("users") == None or len(res["users"]) == 0:
+				break
+			result.extend(res["users"])
+			if res.get("next_cursor") == None or res["next_cursor"] == -1 or res["next_cursor"] == 0:
+				break
+			data["cursor"] = res["next_cursor"]
+		return result
+
+	def GetNotifications(self, count = 20, cursor = None):
+		data = { "count": count }
+		if cursor != None and cursor != "":
+			data["last_cursor"] = cursor
+		return self.GET(2, "info/nontifications", data)
+
+	def Like(self, group_id, chat_id):
+		return self.POST(1, "group/{group_id}/chats/like".format(group_id=group_id), { "id": chat_id })
+
+	def UnLike(self, group_id, chat_id):
+		return self.POST(1, "group/{group_id}/chats/unlike".format(group_id=group_id), { "id": chat_id })
+
+	def Boo(self, group_id, chat_id):
+		return self.POST(1, "group/{group_id}/chats/boo".format(group_id=group_id), { "id": chat_id })
+
+	def UnBoo(self, group_id, chat_id):
+		return self.POST(1, "group/{group_id}/chats/unboo".format(group_id=group_id), { "id": chat_id })
+
+	def Follow(self, user_id):
+		return self.POST(1, "me/contacts", { "users": user_id })
+
+	def UnFollow(self, user_id):
+		return self.POST(1, "me/contacts/remove", { "users": user_id })
+
+	def RemoveChat(self, group_id, chat_id):
+		return self.POST(1, "group/{group_id}/chats/remove".format(group_id=group_id), { "id": chat_id })
+
+	def Join(self, group_id):
+		return self.POST(1, "group/{group_id}/join".format(group_id=group_id), {})
+
+	def RefuseInvitation(self, group_id):
+		return self.POST(1, "group/{group_id}/refuse_invitation".format(group_id=group_id), {})
+
+	def Kick(self, group_id, user_id):
+		return self.POST(1, "group/{group_id}/kick".format(group_id=group_id), { "target_user": user_id })
+
+	def LeaderTransfer(self, group_id, user_id):
+		return self.POST(1, "group/{group_id}/transfer".format(group_id=group_id), { "target_user": user_id })
+
+	def SetSubleader(self, group_id, user_id):
+		return self.POST(1, "group/{group_id}/subleaders".format(group_id=group_id), { "user": user_id })
+
+	def RemoveSubleader(self, group_id, user_id):
+		return self.POST(1, "group/{group_id}/subleaders/remove".format(group_id=group_id), { "user": user_id })
+
+	def Block(self, user_id):
+		return self.POST(1, "me/blocking_users", { "users": user_id })
+
+	def Unblock(self, user_id):
+		return self.POST(1, "me/blocking_users/remove", { "users": user_id })
+
+	def GET(self, version, request_url, query = {}):
+		url = "https://api.lobi.co/{version}/{request_url}?platform={platform}&lang=ja&token={token}&{query}".format(version=version, request_url=request_url, platform=self.platform, token=self.Token, query=urllib.urlencode(query))
+		header = {
+			"User-Agent": self.UserAgent,
+			"Host": "api.lobi.co"
+		}
+		return json.loads(urllib2.urlopen(urllib2.Request(url, None, header)).read())
+
+	def POST(self, version, request_url, query = {}):
+		url = "https://api.lobi.co/{version}/{request_url}".format(version=version, request_url=request_url)
+		header = {
+			"User-Agent": self.UserAgent,
+			"Host": "api.lobi.co"
+		}
+		data = {
+			"lang": "ja",
+			"token": self.Token,
+			"platform": self.platform,
+		}.update(query)
+                return json.loads(urllib2.urlopen(urllib2.Request(url, data, header)).read())
+        def PostMessage(self,group_id,message):
+                url = "https://api.lobi.co/1/group/{group_id}/chats".format(group_id=group_id)
+		data = {
+			"lang": "en",
+                        "message": message,
+			"token": self.Token,
+                        "type": "shout"
+		}
+		response = requests.post(url,data)
+                return response.json()
+        def PostReply(self,group_id,chat_id,message):
+                url = "https://api.lobi.co/1/group/{group_id}/chats".format(group_id=group_id)
+		data = {
+			"lang": "en",
+                        "message": message,
+                        "reply_to": chat_id,
+			"token": self.Token,
+                        "type": "normal"
+		}
+		response = requests.post(url,data)
+                return response.json()
+        def MakePrivateGroup(self,user_id):
+                url = "https://web.lobi.co/api/groups/1on1s"
+		data = {
+                        "user": user_id
+		}
+                headers = {
+                        "Accept":"application/json, text/plain, */*",
+                        "UserAgent":self.UserAgent,
+                        "AcceptLanguage":"ja,en-US;q=0.8,en;q=0.6",
+                        "Accept-Encoding":"gzip, deflate, br",
+                        "Cookie":"_ga=GA1.2.1867834487.1592919202; lnubid=bqL35a1592919210.41835; installId=30122f5ea537671d46eed11047539cc7c87b7b46; __gads=ID=18cea69ad2ac2060:T=1607353223:S=ALNI_MZGI39eC4plNOuVpLYscdewr15-Pw; _gid=GA1.2.2066270152.1609998250; is_signed_in=1; nsess=535496626bacf52b2ba1b1b358cb51c620189c6d; lastReadNotificationId=9526122117; _gat=1"
+                }
+                response = requests.post(url,data,headers = headers)
+                return response.json()
+#class WarGamingAPI:
+#        def 
---
+++ recruitingbot.py
@@ -0,0 +1,106 @@
+# coding: UTF-8
+import collections
+import time
+import datetime
+import json
+import requests
+import decimal
+import sys
+import schedule
+from photon import LobiAPI
+
+class RecruitingBot(LobiAPI):
+        def GetNewMessage(self,group_id):
+                top_thread = self.GetThreads(group_id,1)
+                if top_thread[0].get('replies')==None:
+                        return top_thread
+                else:
+                        return 0
+        def PostFirstReply(self,group_id,chat_id):
+                message = "[BOTより返信]\nWWN広報室へようこそ\n入隊希望の方はこのメッセージにグーを、\nそれ以外の場合はブーをお願いします"
+                response = self.PostReply(group_id,chat_id,message)
+        def GetFirstReplyGooBoo(self,group_id,chat_id):
+                replies = self.GetRepliesAll(group_id,chat_id)
+                first_reply = replies["chats"][0]
+                goo = first_reply["likes_count"]
+                boo = first_reply["boos_count"]
+                if goo == "0" and boo == "0":
+                    return 0
+                elif goo == "1" and boo == "0":
+                    return 1
+                elif goo == "0" and boo == "1":
+                    return 2
+                else:
+                    return 3
+        def PostGooBooError(self,group_id,chat_id):
+                message = "[BOTより返信]\nグーブーの入力が不適切です\nどちらかを一度だけ押してください"
+                response = self.PostReply(group_id,chat_id,message)
+                return response
+        def PostAskIGN(self,group_id,chat_id):
+                message = "[BOTより返信]\n入隊希望ですね\nあなたのIGNを返信欄に入力して下さい\nプログラムで検索しますので入力ミスのないようお願いします"
+                response = self.PostReply(group_id,chat_id,message)
+                return response
+        def PostCallAdmin(self,group_id,chat_id,admin_room_id):
+                message = "[BOTより返信]\n入隊以外の要件ですね\n担当者を呼び出します"
+                response = self.PostReply(group_id,chat_id,message)
+                message = "[BOTより送信]\n広報室に入隊以外の要件の方がいらっしゃいます\n対応お願いします"
+                response = self.PostMessage(admin_room_id,message)
+                return response
+        def GetIGN(self,group_id,chat_id,reply_number):
+                replies = self.GetRepliesAll(group_id,chat_id)
+                replies_count = len(replies["chats"])
+                if replies_count == reply_number:
+                        return replies["chats"][0]["message"]
+                else:
+                        return None
+        def GetIGNData(self,application_id,ign,wg_access_token):
+                url = "https://api.wotblitz.asia/wotb/account/list/?application_id={application_id}&search={ign}".format(application_id=application_id,ign=ign)
+                response = requests.get(url)
+                data = response.json()
+                if data["meta"]["count"] == 0:
+                        return None
+                else:
+                        account_id = data["data"][0]["account_id"]
+                        url = "https://api.wotblitz.asia/wotb/account/info/?application_id={application_id}&account_id={account_id}&access_token={wg_access_token}".format(application_id=application_id,account_id=account_id,wg_access_token=wg_access_token)
+                        response = requests.get(url)
+                        data = response.json()
+                        personal_data = data["data"][str(account_id)]
+                        return personal_data
+        def PostIGNError(self,group_id,chat_id):
+                message = "[BOTより返信]\nIGNにマッチしたデータが見つかりませんでした\n再度IGN入力してください"
+                response = self.PostReply(group_id,chat_id,message)
+                return response
+        def PostAskOK(self,group_id,chat_id,battles,nickname):
+                message = "[BOTより返信]\n検索結果を表示します\nIGN："+ nickname + "\n戦闘数：" + str(battles) + "\n間違いがなければこのメッセージにグーをしてください\n違っていればブーを押してください\n再度IGNを入力できます"
+                response = self.PostReply(group_id,chat_id,message)
+                return response
+        def GetAskOKGooBoo(self,group_id,chat_id):
+                replies = self.GetRepliesAll(group_id,chat_id)
+                first_reply = replies["chats"][0]
+                goo = first_reply["likes_count"]
+                boo = first_reply["boos_count"]
+                if goo == "0" and boo == "0":
+                    return 0
+                elif goo == "1" and boo == "0":
+                    return 1
+                elif goo == "0" and boo == "1":
+                    return 2
+                else:
+                    return 3                
+        def PostWhichClan(self,group_id,chat_id):
+                message = "[BOTより返信]\n所属したいクランの希望を聞きます\n以下の表に従って数字（半角）を返信してください\nWWN：1\nWWN-2：2\nWWN-3：3\nWWN-4：4\nWWN-A：5\nWWN-E：6\n\n"
+                response = self.PostReply(group_id,chat_id,message)
+                return response
+        def GetWhichClan(self,group_id,chat_id,reply_number):
+                replies = self.GetRepliesAll(group_id,chat_id)
+                replies_count = len(replies["chats"])
+                if replies_count == reply_number:
+                        return replies["chats"][0]["message"]
+                else:
+                        return None
+        def PostStartExam(self,group_id,chat_id,admin_room_id,battles,winrate,last_battle_time,nickname,clan):
+                message = "[BOTより返信]\n担当者が審査を開始します\n（この作業には最大数日かかる可能性があります）\n審査ののち担当者がロビーで個チャをいたします\nしばらくお待ちください"
+                response = self.PostReply(group_id,chat_id,message)
+                message = "[BOTより送信]\n広報室より入隊希望の" + nickname + "さん\n基本情報は以下の通りです\n\nIGN：" + nickname + "\n戦闘数：" + str(battles) + "\n勝率：" + str(winrate) + "\n最終戦闘日：" + str(last_battle_time) + "\n\n戦闘名声、ツイッターの確認お願いします\nグー4で規約確認へ\n以後の作業は司令官に引き継ぎます"
+                response = self.PostMessage(admin_room_id,message)
+                return response
